4603c0a2733d4805126932abddb8d7b80fb21d5a
Brad <bradleynielsen@gmail.com>
2016-11-30 16:30:29 -0600

    added some node mods

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,47 @@
 var express = require('express');
 var router = express.Router();
-
+var models = require('../models');
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', {
-  	City: 'Austin' ,
-  	springTemperature: "60 - 82",
-  	springPercipitation: "Wet",
-  	springHumidity: "10% - 20%",
-  	springWind: "12 mph",
+// router.get('/', function(req, res) {
+//   res.render('index', {
+//   	City: 'Austin' ,
+//   	springTemperature: "60 - 82",
+//   	springPercipitation: "Wet",
+//   	springHumidity: "10% - 20%",
+//   	springWind: "12 mph",
+/*=============
 
-  	summerTemperature: "86 - 112",
-  	summerPercipitation: "Very Dry",
-  	summerHumidity: "0% - 3%",
-  	summerWind: "14 mph",
+ ==============*/
 
-  	fallTemperature: "60 - 78",
-  	fallPercipitation: "Very Wet",
-  	fallHumidity: "1% - 2%",
-  	fallWind: "18 mph",
+//   	summerTemperature: "86 - 112",
+//   	summerPercipitation: "Very Dry",
+//   	summerHumidity: "0% - 3%",
+//   	summerWind: "14 mph",
+//
+//   	fallTemperature: "60 - 78",
+//   	fallPercipitation: "Very Wet",
+//   	fallHumidity: "1% - 2%",
+//   	fallWind: "18 mph",
+//
+//   	winterTemperature: "42 - 67",
+//   	winterPercipitation: "Dry",
+//   	winterHumidity: "0% - 2%",
+//   	winterWind: "8 mph",
+//
+// });
+// });
+//
+// router.get('/', function(request, response) {
+// 	response.render('splash', {
+//
+// 	})
 
-  	winterTemperature: "42 - 67",
-  	winterPercipitation: "Dry",
-  	winterHumidity: "0% - 2%",
-  	winterWind: "8 mph",
 
-});
+router.get('/austin', function(request, response) {
+	models.Campmabrymin.findAll().then(function(data){
+		console.log(data);
+	});
 });
 
 
