1e945a843faba31e93b4bb62a1f67fec6b5386ec
Brad <bradleynielsen@gmail.com>
2016-12-03 02:49:40 -0600

    before pull

--- routes/index.js
+++ routes/index.js
@@ -61,45 +61,62 @@ router.post("/city", function(request, response){
         // return response.json(data);
 
 
-        var springtempArray = [];
-        var springprecipArray = [];
-        var springwindArray = [];
+        var springTempArray = [];
+        var springPrecipArray = [];
+        var springWindArray = [];
 
         //Loop over the sql array
         for (var i = 0; i < data.length; i++) {
-            springtempArray.push(data[i].HOURLYDRYBULBTEMPF);
-            springprecipArray.push(data[i].HOURLYPrecip);
-            springwindArray.push(data[i].HOURLYWindSpeed);
+            springTempArray.push(data[i].HOURLYDRYBULBTEMPF);
+            springPrecipArray.push(data[i].HOURLYPrecip);
+            springWindArray.push(data[i].HOURLYWindSpeed);
         }
 
         //
         //Temps
         //
 
-
-        springStats.springTempMean = stats.mean(springtempArray);
-        springStats.springTempMedian = stats.median(springtempArray);
-        springStats.springTempMode = stats.mode(springtempArray);
-        springStats.springTempStdev = stats.stdev(springtempArray);
+        console.log("mean: %s", stats.mean(springTempArray));
+        console.log("median: %s", stats.median(springTempArray));
+        console.log("mode: %s", stats.mode(springTempArray));
+        console.log("standard deviation: %s", stats.stdev(springTempArray));
+        springStats.springTempMean = stats.mean(springTempArray);
+        springStats.springTempMedian = stats.median(springTempArray);
+        springStats.springTempMode = stats.mode(springTempArray);
+        springStats.springTempStdev = stats.stdev(springTempArray);
 
         //
         //Rains
         //
 
-        console.log("mean: %s", stats.mean(springprecipArray));
-        console.log("median: %s", stats.median(springprecipArray));
-        console.log("mode: %s", stats.mode(springprecipArray));
-        console.log("standard deviation: %s", stats.stdev(springprecipArray));
+        console.log("mean: %s", stats.mean(springPrecipArray));
+        console.log("median: %s", stats.median(springPrecipArray));
+        console.log("mode: %s", stats.mode(springPrecipArray));
+        console.log("standard deviation: %s", stats.stdev(springPrecipArray));
+        springStats.springRainMean = stats.mean(springPrecipArray);
+        springStats.springRainMedian = stats.median(springPrecipArray);
+        springStats.springRainMode = stats.mode(springPrecipArray);
+        springStats.springRainStdev = stats.stdev(springPrecipArray);
 
 
         //
         //Winds
         //
 
-        console.log("mean: %s", stats.mean(springwindArray));
-        console.log("median: %s", stats.median(springwindArray));
-        console.log("mode: %s", stats.mode(springwindArray));
-        console.log("standard deviation: %s", stats.stdev(springwindArray));
+        console.log("mean: %s", stats.mean(springWindArray));
+        console.log("median: %s", stats.median(springWindArray));
+        console.log("mode: %s", stats.mode(springWindArray));
+        console.log("standard deviation: %s", stats.stdev(springWindArray));
+        springStats.springWindMean = stats.mean(springWindArray);
+        springStats.springWindMedian = stats.median(springWindArray);
+        springStats.springWindMode = stats.mode(springWindArray);
+        springStats.springWindStdev = stats.stdev(springWindArray);
+
+
+
+
+
+
     }).then(function (data) {
             //Summer
             models.summer.findAll({
@@ -108,9 +125,9 @@ router.post("/city", function(request, response){
                 // console.log(response.json(data));
                 // return response.json(data);
 
-                var summertempArray = [];
-                var summerprecipArray = [];
-                var summerwindArray = [];
+                var summerTempArray = [];
+                var summerPrecipArray = [];
+                var summerWindArray = [];
 
 
                 //Loop over the sql array
@@ -123,36 +140,42 @@ router.post("/city", function(request, response){
                 //
                 //Temps
                 //
-                console.log("=======");
-                console.log("Temps");
-                console.log("=======");
 
-                console.log("mean: %s", stats.mean(summertempArray));
-                console.log("median: %s", stats.median(summertempArray));
-                console.log("mode: %s", stats.mode(summertempArray));
-                console.log("standard deviation: %s", stats.stdev(summertempArray));
+                console.log("mean: %s", stats.mean(summerTempArray));
+                console.log("median: %s", stats.median(summerTempArray));
+                console.log("mode: %s", stats.mode(summerTempArray));
+                console.log("standard deviation: %s", stats.stdev(summerTempArray));
+                springStats.springTempMean = stats.mean(summerTempArray);
+                springStats.springTempMedian = stats.median(summerTempArray);
+                springStats.springTempMode = stats.mode(summerTempArray);
+                springStats.springTempStdev = stats.stdev(summerTempArray);
 
                 //
                 //Rains
                 //
-                console.log("=======");
-                console.log("Rains");
-                console.log("=======");
-                console.log("mean: %s", stats.mean(summerprecipArray));
-                console.log("median: %s", stats.median(summerprecipArray));
-                console.log("mode: %s", stats.mode(summerprecipArray));
-                console.log("standard deviation: %s", stats.stdev(summerprecipArray));
+
+                console.log("mean: %s", stats.mean(summerPrecipArray));
+                console.log("median: %s", stats.median(summerPrecipArray));
+                console.log("mode: %s", stats.mode(summerPrecipArray));
+                console.log("standard deviation: %s", stats.stdev(summerPrecipArray));
+                springStats.springRainMean = stats.mean(summerPrecipArray);
+                springStats.springRainMedian = stats.median(summerPrecipArray);
+                springStats.springRainMode = stats.mode(summerPrecipArray);
+                springStats.springRainStdev = stats.stdev(summerPrecipArray);
+
 
                 //
                 //Winds
                 //
-                console.log("=======");
-                console.log("Winds");
-                console.log("=======");
-                console.log("mean: %s", stats.mean(summerwindArray));
-                console.log("median: %s", stats.median(summerwindArray));
-                console.log("mode: %s", stats.mode(summerwindArray));
-                console.log("standard deviation: %s", stats.stdev(summerwindArray));
+
+                console.log("mean: %s", stats.mean(summerWindArray));
+                console.log("median: %s", stats.median(summerWindArray));
+                console.log("mode: %s", stats.mode(summerWindArray));
+                console.log("standard deviation: %s", stats.stdev(summerWindArray));
+                springStats.springWindMean = stats.mean(summerWindArray);
+                springStats.springWindMedian = stats.median(summerWindArray);
+                springStats.springWindMode = stats.mode(summerWindArray);
+                springStats.springWindStdev = stats.stdev(summerWindArray);
             })
         }).then(function(data) {
         //Fall
